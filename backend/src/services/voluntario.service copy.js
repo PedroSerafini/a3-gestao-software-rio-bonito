3e2848920d547cfe7a0a90ff1892690a10254b06
Pedro Henrique Serafini <172211008@ulife.com.br>
2025-11-17 20:23:12 -0300

    exemplo

---
+++ backend/src/services/voluntario.service copy.js
@@ -0,0 +1,36 @@
+const cepService = require('./cep.service');
+const voluntarioRepository = require('../repositories/voluntario.repository');
+
+async function criarVoluntario(dados) {
+  const { nome, cpf, celular, cep } = dados;
+
+  if (!nome || !cpf || !celular || !cep) {
+    throw new Error('Dados incompletos (Nome, CPF, Celular e CEP são obrigatórios)');
+  }
+
+  const endereco = await cepService.buscarEnderecoPorCep(cep);
+
+  const voluntarioCompleto = {
+    nome,
+    cpf,
+    celular,
+    cep: endereco.cep,
+    logradouro: endereco.logradouro,
+    bairro: endereco.bairro,
+    cidade: endereco.localidade,
+    uf: endereco.uf,
+  };
+
+  const voluntarioSalvo = await voluntarioRepository.save(voluntarioCompleto);
+
+  return voluntarioSalvo;
+}
+
+async function listarVoluntarios() {
+  return voluntarioRepository.getAll();
+}
+
+module.exports = {
+  criarVoluntario,
+  listarVoluntarios,
+};
