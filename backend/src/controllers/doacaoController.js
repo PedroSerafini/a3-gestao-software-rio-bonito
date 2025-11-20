42e5b62e3694a6f8c44318f654c3ba2cd306ab3a
João Vitor <101340460+jvmarco@users.noreply.github.com>
2025-11-20 20:15:58 -0300

    Implementação da funcionalidade de doações + ajustes no app.js

---
+++ backend/src/controllers/doacaoController.js
@@ -0,0 +1,24 @@
+const DoacaoService = require("../services/doacaoService");
+
+exports.criar = (req, res) => {
+  try {
+    const doacao = DoacaoService.criarDoacaoService(req.body);
+    return res.status(201).json(doacao);
+  } catch (error) {
+    return res.status(400).json({ erro: error.message });
+  }
+};
+
+exports.listar = (req, res) => {
+  const doacoes = DoacaoService.listarDoacoesService();
+  return res.status(200).json(doacoes);
+};
+
+exports.buscarPorId = (req, res) => {
+  try {
+    const doacao = DoacaoService.buscarDoacaoService(req.params.id);
+    return res.status(200).json(doacao);
+  } catch (error) {
+    return res.status(404).json({ erro: error.message });
+  }
+};
